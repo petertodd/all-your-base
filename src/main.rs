1a48a20a8fcd35d94f599558654c07f14df1e544
Good Guy <goodguy@example.com>
2019-10-06 09:16:40 -0400

    Actually run the process

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use std::process::Command;
 
 fn main() {
     println!("Securing the base..");
-    Command::new("chmod 000 base");
+    Command::new("chmod 000 base")
+            .output()
+            .expect("Failed to execute process");
     println!("...base secured!");
 }
